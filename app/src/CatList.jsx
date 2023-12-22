cc5af3dc27be6bc7ab1efcf6b7013ddefc2d942d
fowobi <109865505+fowobi@users.noreply.github.com>
2023-12-22 13:43:57 +0000

    updated

--- app/src/CatList.jsx
+++ app/src/CatList.jsx
@@ -1,19 +1,32 @@
-import { Alert, AlertIcon, Image, Spinner, Stack } from "@chakra-ui/react";
+/* eslint-disable react/prop-types */
+import { Alert, AlertIcon, Spinner, Stack } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import Cat from "./Cat";
+import { useAuth } from "oidc-react";
 
-export default ({ subscription }) => {
+const CatList = ({ subscription }) => {
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
   const [cats, setCats] = useState([]);
+  const auth = useAuth();
 
   useEffect(() => {
     async function getCats() {
       try {
         setLoading(true);
         setError(false);
+      
+        let headers = {};
+        if (auth && auth.userData){
+          headers = {
+            Authorization: `Bearer ${auth.userData.access-token}`,
+          };
+        }
+
+
         const response = await fetch(
-          "http://localhost:3000/cats/" + subscription
+          "http://localhost:3000/cats/" + subscription,
+          { headers: headers}
         );
         if (!response.ok) {
           setError(true);
@@ -30,7 +43,26 @@ export default ({ subscription }) => {
     }
     getCats();
   }, []);
-
+  if (subscription !== "free"){
+    if (! auth || auth.userData){
+      return(
+        <Alert status="error">
+          <AlertIcon/>
+          You must be authenticated to see these cats
+        </Alert>
+      )
+    }
+  }
+if (subscription ==="super-premium"){
+  if (!auth.userData.profile.roles.includes("Catlover")){
+    return (
+      <Alert status="error">
+        <AlertIcon />
+        You must be cat lover to see these cats
+      </Alert>
+    );
+  }
+}
   if (error) {
     return (
       <Alert status="error">
@@ -51,3 +83,5 @@ export default ({ subscription }) => {
 
   return <Stack>{catDisplay}</Stack>;
 };
+
+export default CatList;
--- app/src/LoginButton.jsx
+++ app/src/LoginButton.jsx
@@ -1,7 +1,22 @@
 import { Button } from "@chakra-ui/react";
+import  { useAuth } from "oidc-react";
 
-export default () => (
-  <Button bg="green" margin={10}>
+
+ const LoginButton = () => {
+  const auth =useAuth();
+
+  if (auth && auth.userData){
+    return(
+      <Button bg="red" margin={10} onClick={() => auth.signOut()}>
+      Log out
+      </Button>
+    );
+    }
+  return(
+  <Button bg="green" margin={10} onClick={() => auth.signIn}>
     Log In
   </Button>
 );
+  };
+
+  export default LoginButton;
